76a582afb291f68b90a9208882476d48625ed618
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-07 15:15:07 +0100

    Fetching news from API using stepzen graphQL

---
+++ lib/fetchNews.ts
@@ -0,0 +1,78 @@
+import { gql } from "graphql-request";
+import sortNewsByImage from "./sortNewsByImage";
+
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  // GraphQL query
+  const query = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "gb"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        data {
+          author
+          category
+          image
+          description
+          country
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+      }
+    }
+  `;
+
+  //Fetch function with Next.js 13 caching
+  const res = await fetch(
+    "https://seremban.stepzen.net/api/virtuous-panther/__graphql",
+    {
+      method: "POST",
+      cache: isDynamic ? "no-cache" : "default",
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+
+  console.log("LOADING FROM API FOR CAT >>>", category, keywords);
+
+  const newsResponse = await res.json();
+
+  // Sort function by image vs no image
+  const news = sortNewsByImage(newsResponse.data.myQuery);
+
+  // Return news
+  return news;
+};
+
+export default fetchNews;
