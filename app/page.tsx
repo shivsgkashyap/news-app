ab7cc432d79aad1b558814bb3ff96c3dbee8a8f7
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-07 16:10:32 +0100

    Passed fetched news data to News List component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import { categories } from "@/constants";
 import fetchNews from "@/lib/fetchNews";
+import NewsList from "./NewsList";
+import response from "../response.json";
 
 const Homepage = async () => {
   // fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(",")));
 
-  return <div></div>;
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 };
 
 export default Homepage;
