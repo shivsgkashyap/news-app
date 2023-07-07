4ffe428f2a4ad82db47ff3a9d7b44e33ee56433e
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-07 18:53:35 +0100

    Prebuilding of category page to speed up load

--- app/news/[category]/page.tsx
+++ app/news/[category]/page.tsx
@@ -1,5 +1,6 @@
 import fetchNews from "@/lib/fetchNews";
 import NewsList from "@/app/NewsList";
+import { categories } from "@/constants";
 
 type Props = {
   params: { category: Category };
@@ -17,3 +18,9 @@ const NewsCategory = async ({ params: { category } }: Props) => {
 };
 
 export default NewsCategory;
+
+export async function generateStaticParams() {
+  return categories.map((category) => ({
+    category: category,
+  }));
+}
